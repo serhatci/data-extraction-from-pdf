79c71261c8fd4c616de74e1f0bb1913ca4031fc2
serhat <serhatciftci@gmail.com>
2020-11-28 20:56:52 +0100

    Add get_data_lines_from_text test

--- tests/test_pdf_data_extractor.py
+++ tests/test_pdf_data_extractor.py
@@ -19,9 +19,37 @@ class TestPdfDataExtractor(unittest.TestCase):
         res = src.check_unwanted_keywords_in_the_line(line)
         self.assertEqual(res, 'Records Exposed:')
 
-        line = 'Breached Entity: State Published Date Breach Category'
+        line = 'Breached Entity: State Published Date Breach  Category'
         res = src.check_unwanted_keywords_in_the_line(line)
         self.assertEqual(res, 'Breached Entity:')
+
+        line = 'No unwanted keyword in this text'
+        res = src.check_unwanted_keywords_in_the_line(line)
+        self.assertEqual(res, None)
+
+        line = ''
+        res = src.check_unwanted_keywords_in_the_line(line)
+        self.assertEqual(res, None)
+
+    def test_get_data_lines_from_text(self):
+        text = 'Identity Theft\n' \
+            '2018 Breach List:\n' \
+            'Records Exposed:\n' \
+            'Breached Entity:\n'  \
+            'California Department of Insurance\n'  \
+            '("CDI")\n' \
+            'Source: www.databreaches.net\n' \
+            'URL:  # https://www.databreaches.net\n' \
+            'Breached Entity:\n'
+        eq = [['California Department of Insurance',
+               '("CDI")',
+               'Source: www.databreaches.net',
+               'URL:  # https://www.databreaches.net']]
+        res = src.get_data_lines_from_text(text)
+        self.assertEqual(res[0][0], eq[0][0])
+        self.assertEqual(res[0][1], eq[0][1])
+        self.assertEqual(res[0][2], eq[0][2])
+        self.assertEqual(res[0][3], eq[0][3])
 
 
 if __name__ == '__main__':
